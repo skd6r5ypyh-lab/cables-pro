2ba6ff49d76f7854569360c6ac7add69d9d3d6a6
skd6r5ypyh-lab <skd6r5ypyh@privaterelay.appleid.com>
2026-07-26 20:54:56 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='cables-pro-v11-0-0';
-const FILES=['./','index.html','styles.css','app.js','manifest.webmanifest','logo.png','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const q=r.clone();caches.open(CACHE).then(c=>c.put(e.request,q));return r}).catch(()=>caches.match('index.html'))))});
